1f0370879579d7636e55e786f0136f588c231ba2
Kevin Goslar <kevin.goslar@originate.com>
2013-02-20 15:14:49 -0800

    Dummy app in spec/dummy

---
+++ spec/dummy/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dummy::Application.config.secret_token = '16119c397a0aba96f5279a4b7a8bb138cfe47ccefa0990409dbbdcef2eb00410a758499590f3e8cf89a15172ea8e30826aa946d8f76bc3ed33056f712f0481a8'
---
+++ spec/dummy/config/routes.rb
@@ -0,0 +1,3 @@
+Rails.application.routes.draw do
+  mount Flms::Engine => "/flms"
+end
