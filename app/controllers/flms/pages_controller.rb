04b7ca8bba925036ca4c27740c1e68b12a691cbd
Kevin Goslar <kevin.goslar@originate.com>
2013-02-20 18:12:57 -0800

    Makes creating pages work in the dummy app.

--- app/controllers/flms/pages_controller.rb
+++ app/controllers/flms/pages_controller.rb
@@ -13,30 +13,35 @@ module Flms
     end
 
     def new
+      @page = Page.new
     end
 
     def edit
+      @page = Page.find_by_url params[:id]
     end
 
     def create
+      @page = Page.new params[:page]
       if @page.save
-        redirect_to admin_pages_path, notice: 'Page was successfully created.'
+        redirect_to pages_path, notice: 'Page was successfully created.'
       else
         render action: "new"
       end
     end
 
     def update
+      @page = Page.find_by_url params[:id]
       if @page.update_attributes params[:page]
-        redirect_to admin_pages_path, notice: 'Page was successfully updated.'
+        redirect_to pages_path, notice: 'Page was successfully updated.'
       else
         render action: "edit"
       end
     end
 
     def destroy
+      @page = Page.find_by_url params[:id]
       @page.destroy
-      redirect_to admin_pages_url
+      redirect_to pages_url
     end
   end
 end
