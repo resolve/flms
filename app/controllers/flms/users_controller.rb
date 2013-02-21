0ae30add6d9977c62320852a0065b76e5e4e4edb
Kevin Goslar <kevin.goslar@originate.com>
2013-02-20 18:06:06 -0800

    Makes the user view work in the dummy app.

---
+++ app/controllers/flms/users_controller.rb
@@ -0,0 +1,31 @@
+require_dependency "flms/application_controller"
+
+module Flms
+  class UsersController < ApplicationController
+    #load_and_authorize_resource
+    layout 'flms/admin'
+
+    def index
+      @users = User.all
+    end
+
+    def new
+      @user = User.new
+    end
+
+    def create
+      @user = User.new params[:user]
+      if @user.save
+        redirect_to users_path, notice: 'User created.'
+      else
+        render action: "new"
+      end
+    end
+
+    def destroy
+      @user = User.find params[:id]
+      @user.destroy
+      redirect_to users_path
+    end
+  end
+end
