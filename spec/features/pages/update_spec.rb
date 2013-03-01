0318befd7c5977b30f1b6584695c31a969604a44
Kevin Goslar <kevin.goslar@originate.com>
2013-02-28 17:16:39 -0800

    Updating pages.

---
+++ spec/features/pages/update_spec.rb
@@ -0,0 +1,17 @@
+require 'spec_helper'
+
+feature 'Pages > Create' do
+  test_helpers
+
+  scenario 'allows to create a new page' do
+    capybara_sign_in user_1
+    visit "/flms/pages/#{page_1.url}/edit"
+    fill_in 'Title', with: 'my new title'
+    fill_in 'Url', with: 'my new url'
+    click_button 'Update Page'
+    expect(page_1.reload.title).to eql 'my new title'
+    expect(page_1.url).to eql 'my new url'
+  end
+end
+
+
