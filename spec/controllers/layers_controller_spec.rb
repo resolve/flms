95e0440c102c2acaa2963a112ebb05535686e307
Kevin Goslar <kevin.goslar@originate.com>
2013-03-15 17:32:39 -0700

    Fixes unit tests.

--- spec/controllers/layers_controller_spec.rb
+++ spec/controllers/layers_controller_spec.rb
@@ -5,9 +5,9 @@ describe Flms::LayersController do
 
   describe 'delete' do
     describe 'access control' do
-      let(:request) { delete :destroy, id: block_1a.id, page_id: page_1.url, use_route: :flms }
-      let(:access_granted_check) { response.status == 302 && response.location == "http://test.host/flms/pages/#{page_1.url}/blocks" }
-      let(:database_performed_check) { Flms::Block.count == 0 }
+      let(:request) { delete :destroy, id: image_layer_1a1.id, block_id: block_1a.id, page_id: page_1.url, use_route: :flms }
+      let(:access_granted_check) { response.status == 302 && response.location == "http://test.host/flms/pages/#{page_1.url}/blocks/#{block_1a.id}" }
+      let(:database_performed_check) { Flms::Layer.count == 0 }
       it_should_behave_like 'an action accessible only to logged-in users'
     end
 
