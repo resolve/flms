df046bdb436a038ec9f8d67889e5762d1bc43063
dhawt <daniel@eduschedu.com>
2013-04-01 09:41:07 -0700

    Add ability for user to specify layer position in start and end state using friendly position names; handle translation to CSS x,y coordinates under the hood

--- app/models/flms/keyframe.rb
+++ app/models/flms/keyframe.rb
@@ -5,7 +5,7 @@ module Flms
                     :width, :height,
                     :position_x, :position_y,
                     :opacity, :scale, :blur,
-                    :opacity_percent, :scale_percent, :blur_percent
+                    :opacity_percent, :scale_percent, :blur_percent, :position_name
 
     belongs_to :layer
 
@@ -15,6 +15,29 @@ module Flms
     validates_numericality_of :height, greater_than_or_equal_to: 0, allow_nil: true
     validates_numericality_of :opacity, greater_than_or_equal_to: 0, less_than_or_equal_to: 1, allow_nil: true
     validates_numericality_of :scale, greater_than_or_equal_to: 0, allow_nil: true
+
+    # Flms provides some 'friendly' position values to hide the details of setting x,y values for CSS positioning of layers.
+    # Below we store a list of valid position names, and corresponding x,y coordinates for each
+    POSITION_NAMES = %w[center top-left top top-right right bottom-right bottom bottom-left left]
+    POSITION_COORDINATES = [[0.5, 0.5], [0.0, 0.0], [0.5, 0.0], [1.0, 0.0], [1.0, 0.5], [1.0, 1.0], [0.5, 1.0], [0.0, 1.0], [0.0, 0.5]]
+
+    def position_name_valid? position_name
+      POSITION_NAMES.include?(position_name)
+    end
+
+    def position_coordinates_valid? position_coordinates
+      POSITION_COORDINATES.include? position_coordinates
+    end
+
+    def position_name_to_coordinates position_name
+      raise 'position name invalid' unless position_name_valid?(position_name)
+      POSITION_COORDINATES[POSITION_NAMES.index(position_name)]
+    end
+
+    def position_coordinates_to_name position_coordinates
+      raise 'position coordinates invalid' unless position_coordinates_valid?(position_coordinates)
+      POSITION_NAMES[POSITION_COORDINATES.index(position_coordinates)]
+    end
 
     def opacity_percent
       (self.opacity * 100).to_i
@@ -38,6 +61,16 @@ module Flms
 
     def blur_percent= val
       self.blur = val.to_i / 100.0
+    end
+
+    def position_name
+      position_coordinates_to_name [self.position_x, self.position_y]
+    end
+
+    def position_name= val
+      coordinates = position_name_to_coordinates val
+      self.position_x = coordinates[0]
+      self.position_y = coordinates[1]
     end
 
   end
--- spec/models/keyframe_spec.rb
+++ spec/models/keyframe_spec.rb
@@ -31,6 +31,95 @@ describe Flms::Keyframe do
         expect(Flms::Keyframe.new(blur: 0.5).blur_percent).to eql 50
       end
     end
+
+    describe 'position_name' do
+      it 'sets position coordinates from name correctly' do
+        keyframe = Flms::Keyframe.new(position_name: 'top-right')
+        expect(keyframe.position_x).to eql 1.0
+        expect(keyframe.position_y).to eql 0.0
+      end
+
+      it 'gets position name from coordinates correctly' do
+        keyframe = Flms::Keyframe.new(position_x: 1.0, position_y: 0.0)
+        expect(keyframe.position_name).to eql 'top-right'
+      end
+    end
+  end
+
+  describe 'positions' do
+    let(:keyframe) { Flms::Keyframe.new }
+
+    describe 'position_name_valid?' do
+      it 'returns true for all valid position names' do
+        expect(keyframe.position_name_valid? 'center').to eql true
+        expect(keyframe.position_name_valid? 'top-left').to eql true
+        expect(keyframe.position_name_valid? 'top').to eql true
+        expect(keyframe.position_name_valid? 'top-right').to eql true
+        expect(keyframe.position_name_valid? 'right').to eql true
+        expect(keyframe.position_name_valid? 'bottom-right').to eql true
+        expect(keyframe.position_name_valid? 'bottom').to eql true
+        expect(keyframe.position_name_valid? 'bottom-left').to eql true
+        expect(keyframe.position_name_valid? 'left').to eql true
+      end
+
+      it 'returns false for a bad position name' do
+        expect(keyframe.position_name_valid? 'an unknown position name').to eql false
+      end
+    end
+    
+    describe 'position_coordiates_valid?' do
+      it 'returns true for all valid position coordinates' do
+        expect(keyframe.position_coordinates_valid? [0.5, 0.5]).to eql true
+        expect(keyframe.position_coordinates_valid? [0.0, 0.0]).to eql true
+        expect(keyframe.position_coordinates_valid? [0.5, 0.0]).to eql true
+        expect(keyframe.position_coordinates_valid? [1.0, 0.0]).to eql true
+        expect(keyframe.position_coordinates_valid? [1.0, 0.5]).to eql true
+        expect(keyframe.position_coordinates_valid? [1.0, 1.0]).to eql true
+        expect(keyframe.position_coordinates_valid? [0.5, 1.0]).to eql true
+        expect(keyframe.position_coordinates_valid? [0.0, 1.0]).to eql true
+        expect(keyframe.position_coordinates_valid? [0.0, 0.5]).to eql true
+      end
+      
+      it 'returns false for an unknown set of coordinates' do
+        expect(keyframe.position_coordinates_valid? 'an unknown set of coordinates').to eql false
+      end
+    end
+
+    describe 'position_name_to_coordinates' do
+      it 'provides correct x,y coordinates for valid position names' do
+        expect(keyframe.position_name_to_coordinates 'center').to eql [0.5, 0.5]
+        expect(keyframe.position_name_to_coordinates 'top-left').to eql [0.0, 0.0]
+        expect(keyframe.position_name_to_coordinates 'top').to eql [0.5, 0.0]
+        expect(keyframe.position_name_to_coordinates 'top-right').to eql [1.0, 0.0]
+        expect(keyframe.position_name_to_coordinates 'right').to eql [1.0, 0.5]
+        expect(keyframe.position_name_to_coordinates 'bottom-right').to eql [1.0, 1.0]
+        expect(keyframe.position_name_to_coordinates 'bottom').to eql [0.5, 1.0]
+        expect(keyframe.position_name_to_coordinates 'bottom-left').to eql [0.0, 1.0]
+        expect(keyframe.position_name_to_coordinates 'left').to eql [0.0, 0.5]
+      end
+
+      it 'raises an error when given an invalid position name' do
+        expect { keyframe.position_name_to_coordinates 'an invalid position name' }.to raise_error
+      end
+    end
+
+    describe 'position_coordinates_to_name' do
+      it 'provides correct name from x,y coordinates' do
+        expect(keyframe.position_coordinates_to_name [0.5, 0.5]).to eql 'center'
+        expect(keyframe.position_coordinates_to_name [0.0, 0.0]).to eql 'top-left'
+        expect(keyframe.position_coordinates_to_name [0.5, 0.0]).to eql 'top'
+        expect(keyframe.position_coordinates_to_name [1.0, 0.0]).to eql 'top-right'
+        expect(keyframe.position_coordinates_to_name [1.0, 0.5]).to eql 'right'
+        expect(keyframe.position_coordinates_to_name [1.0, 1.0]).to eql 'bottom-right'
+        expect(keyframe.position_coordinates_to_name [0.5, 1.0]).to eql 'bottom'
+        expect(keyframe.position_coordinates_to_name [0.0, 1.0]).to eql 'bottom-left'
+        expect(keyframe.position_coordinates_to_name [0.0, 0.5]).to eql 'left'
+      end
+
+      it 'raises an error when given coordinates that do not correspond to a known position' do
+        expect { keyframe.position_coordinates_to_name [-1.0, -1.0] }.to raise_error
+      end
+    end
   end
 
 end
