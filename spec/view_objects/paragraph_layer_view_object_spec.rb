13d7fd168a1da571a96f00074337789a77eff0c9
dhawt <daniel@eduschedu.com>
2013-04-25 11:02:34 -0700

    Paragraph layers now honor text color attribute.

--- spec/view_objects/paragraph_layer_view_object_spec.rb
+++ spec/view_objects/paragraph_layer_view_object_spec.rb
@@ -1,12 +1,17 @@
 require 'spec_helper'
 
 describe Flms::ParagraphLayerViewObject do
-  let(:paragraph_layer) { create :paragraph_layer,
+  let(:paragraph_layer) { create :paragraph_layer, color: 'abc',
                                  header: 'header text', body: 'body text' }
   let(:subject) { Flms::ParagraphLayerViewObject.new(paragraph_layer) }
 
   describe 'attributes' do
     let(:data) { subject.attributes }
+
+    it 'should include color styling' do
+      expect(data).to have_key :style
+      expect(data[:style]).to match 'color: #abc;'
+    end
 
     it 'should include basic attributes' do
       expect(data).to have_key 'data-0'
