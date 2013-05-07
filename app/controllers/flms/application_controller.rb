dad00fc1ed3b26abd238b4c52305902f86a4c1ac
dhawt <daniel@eduschedu.com>
2013-05-07 15:38:47 -0700

    Refactor to use yield instead of call(block) in with_format method.

--- app/controllers/flms/application_controller.rb
+++ app/controllers/flms/application_controller.rb
@@ -5,10 +5,10 @@ module Flms
     # format to a view, we need this method to swap the format to html so that
     # Rails can properly resolve the partials (since we use *.html.haml for both
     # :html and :plain_html formats)
-    def with_format(format, &block)
+    def with_format format
       old_formats = formats
       self.formats = [format]
-      block.call
+      yield
       self.formats = old_formats
       nil
     end
