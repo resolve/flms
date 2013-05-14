316050a574f4b1f0748783b810210bf15cd1e709
dhawt <daniel@eduschedu.com>
2013-05-14 12:37:57 -0700

    Remove .thumb div on block nav, use larger-resolution thumbnail image for nav.

--- spec/features/pages/show_spec.rb
+++ spec/features/pages/show_spec.rb
@@ -22,7 +22,7 @@ feature 'Pages > Show' do
     image_layer_1a1
     image_layer_1c1
     visit "/flms/pages/#{page_1.url}"
-    expect(page).to have_selector ".thumb\##{ block_1a.name }"
+    expect(page).to have_selector "li\##{ block_1a.name }"
     expect(page).to have_selector ".name\##{ block_1a.name }"
     expect(page).to have_selector "a\##{ block_1a.name }"
     expect(page).to have_selector "a\##{ block_1c.name }"
